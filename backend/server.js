471ccb430ebbe1318d35b75e18f3182ca4eb5809
sahitya somala <sahityasomala57@gmail.com>
2026-04-03 16:09:20 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/menu', menuRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/orders', orderRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
